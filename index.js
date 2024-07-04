13fb391915eb1b664ab9a12a4c9091a83751bdbd
shukri1236 <shukri1236>
2024-07-04 22:43:16 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+// Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+   window.customerName = window.customerName.toUpperCase();
+}
+function setBestCustomer(){
+   window.bestCustomer = "not bob";
+}
+function overwriteBestCustomer(){
+   window.bestCustomer = "maybe bob";
+}
+const leastFavoriteCustomer = "Kim"
+function changeLeastFavoriteCustomer(){
+   leastFavoriteCustomer = "bob"
+}
